a814fc2395597019663b34f8bba4915372c46535
Thomas <thomasblengino@hotmail.fr>
2022-07-17 16:42:57 +0200

    fin interface tableau de bord

---
+++ Projet_Memoire/Assets/Scripts/Interfaces/JaugeVitesse.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JaugeVitesse : MonoBehaviour
+{
+    [SerializeField] VehicleManager _VehicleManager;
+    [SerializeField] GameObject textfieldSpeed;
+    [SerializeField] GameObject JaugeSpeed;
+    [SerializeField] float offsetJauge;
+
+    private void Start()
+    {
+        JaugeSpeed.GetComponent<Image>().fillAmount = offsetJauge;
+    }
+    void Update()
+    {
+        textfieldSpeed.GetComponent<TMP_Text>().text = Mathf.Round(_VehicleManager._VehicleMovement.currentSpeed).ToString() + " km/h";
+        JaugeSpeed.GetComponent<Image>().fillAmount = (_VehicleManager._VehicleMovement.currentSpeed/ _VehicleManager._VehicleMovement.currentMaxSpeed) + offsetJauge;
+    }
+}
